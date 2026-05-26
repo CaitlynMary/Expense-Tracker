5b1a16f22d7e45450232f0ba5f8173934661216c
CaitlynMary <caitlynmary2004@gmail.com>
2026-05-26 14:10:18 +0530

    Updated production backend URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://expense-tracker-backend.onrender.com/api' // You will replace this with your actual Render URL
+  apiUrl: 'https://expense-tracker-backend-r38x.onrender.com/api'
 };
